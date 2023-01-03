cd244111e9af55d3e509c4188afa34e4bc962e48
Your Name <chai.syuen@gmail.com>
2023-01-03 12:38:30 +0800

    splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -14,19 +14,6 @@ import './screens/path_overview_screen.dart';
 import './screens/directions_screen.dart';
 import './screens/start_trace_screen.dart';
 
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await Firebase.initializeApp(
-//     options: DefaultFirebaseOptions.currentPlatform,
-//   );
-//   SystemChrome.setPreferredOrientations([
-//     DeviceOrientation.portraitDown,
-//     DeviceOrientation.portraitUp,
-//   ]);
-
-//   runApp(MyApp());
-// }
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
@@ -57,55 +44,7 @@ class MyApp extends StatelessWidget {
       child: Consumer<AppTheme>(
         builder: (context, theme, _) => MaterialApp(
           title: 'Flutter Demo',
-          // darkTheme: ThemeData(
-          //   scaffoldBackgroundColor: Color.fromARGB(255, 0, 0, 0),
-          //   appBarTheme: const AppBarTheme(color: themeColor),
-          //   primaryColor: themeColor,
-          //   primaryColorDark: Color.fromARGB(255, 207, 30, 148),
-          //   errorColor: Colors.redAccent,
-          //   elevatedButtonTheme: ElevatedButtonThemeData(
-          //       style: ElevatedButton.styleFrom(
-          //     backgroundColor: Color.fromARGB(255, 255, 40, 183),
-          //     elevation: 5,
-          //   )),
-          //   inputDecorationTheme: const InputDecorationTheme(
-          //     focusedBorder: OutlineInputBorder(
-          //       borderSide: BorderSide(color: themeColor, width: 2),
-          //     ),
-          //     floatingLabelStyle: TextStyle(
-          //       color: themeColor,
-          //     ),
-          //   ),
-          //   colorScheme: ColorScheme.fromSwatch().copyWith(
-          //     secondary: Color.fromARGB(255, 195, 0, 130), // Your accent color
-          //   ),
-          // ),
-          // theme: ThemeData(
-          //   scaffoldBackgroundColor: Colors.white,
-          //   appBarTheme: const AppBarTheme(color: themeColor),
-          //   primaryColor: themeColor,
-          //   primaryColorDark: Color.fromARGB(255, 207, 30, 148),
-          //   errorColor: Colors.redAccent,
-          //   elevatedButtonTheme: ElevatedButtonThemeData(
-          //       style: ElevatedButton.styleFrom(
-          //     backgroundColor: Color.fromARGB(255, 255, 40, 183),
-          //     elevation: 5,
-          //   )),
-          //   inputDecorationTheme: const InputDecorationTheme(
-          //     focusedBorder: OutlineInputBorder(
-          //       borderSide: BorderSide(color: themeColor, width: 2),
-          //     ),
-          //     floatingLabelStyle: TextStyle(
-          //       color: themeColor,
-          //     ),
-          //   ),
-          //   colorScheme: ColorScheme.fromSwatch().copyWith(
-          //     secondary: Color.fromARGB(255, 195, 0, 130), // Your accent color
-          //   ),
-          // ),
           theme: theme.getTheme(),
-
-          // home: const MyHomePage(title: 'Flutter Demo Home Page'),
           initialRoute: '/',
           routes: {
             '/': (ctx) => BottomNavigationScreen(),
